de0df6259cd1fbc29ff286160af3a9d24febbad7
Ben Hmida Aya <130604514+AyaHmida@users.noreply.github.com>
2024-04-16 20:15:51 +0100

    Update FollowController.php

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Follower;
 use App\Models\User;
+use App\Notifications\FollowDBNotify;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
