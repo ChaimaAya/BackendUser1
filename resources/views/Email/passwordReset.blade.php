aa8d459b37595968c78ce1648c3e5bfb56ace2ed
aya <ayahmida64@gmail.com>
2024-05-13 22:43:54 +0100

    chatUpdate

--- resources/views/Email/passwordReset.blade.php
+++ resources/views/Email/passwordReset.blade.php
@@ -3,7 +3,7 @@
 
 Vous recevez cet e-mail car nous avons reçu une demande de réinitialisation du mot de passe pour votre compte.
 <x-mail::button :url="'http://localhost:3000/' . $token ">
-Reset Password</x-mail::button>
+réinitialiser le mot de passe</x-mail::button>
 
 Thanks
 </x-mail::message>
